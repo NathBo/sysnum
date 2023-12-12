bac855f8b27cc9da96292116f64be7f82663ad4d
NathBo <nboyer200492@gmail.com>
2023-12-12 15:55:27 +0100

    alu.net

--- lib/netlists/main.py
+++ lib/netlists/main.py
@@ -1,3 +1,6 @@
+from lib_carotte import *
+
+
 def full_adder(a, b, c):
     tmp = a ^ b
     return (tmp ^ c, (tmp & c) | (a & b))
@@ -23,15 +26,16 @@ def n_not(a):
 def mux16(a,b,c):
     assert(a.bus_size == 16 and b.bus_size == 16)
     rep = Mux(a[0],b[0],c) 
-    for i in range(16):
+    for i in range(1,16):
         rep_i = Mux(a[i],b[i],c)
         rep = rep + rep_i
+    return rep
 
 
 
 def alu(a,b,sub):
     bnot = n_not(b)
-    moins_b = n_adder(bnot,Constant("0000000000000001"))       #y a 15 0 en tout
+    moins_b,nul = n_adder(bnot,Constant("0000000000000001"))       #y a 15 0 en tout
     bon_b = mux16(b,moins_b,sub)
     (s,c) = n_adder(a,bon_b)
     return (s,c)
