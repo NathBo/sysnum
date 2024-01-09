a5cea6253102956d56da2dbe7710b3f4f4965d86
mateo713 <68335444+mateo713@users.noreply.github.com>
2024-01-09 16:25:50 +0100

    Update digit_maker.py

--- lib/simulator/digit_maker.py
+++ lib/simulator/digit_maker.py
@@ -1,19 +1,22 @@
-nlin, ncol = 12, 8
-Nlin = 108
+nlin, ncol = 12, 8 #taille d'un chiffre
+Nlin = 108 #longueur d'une ligne, en mégapixels
+
+
+#écrire le design d'un chiffre, et le script donne le code OCaml de la liste des positions, à laquelle on peut rajouter un offset pour déplacer le chiffre
 
 template = [
-    [0, 0, 0, 1, 0, 0, 0, 0],
-    [0, 0, 1, 0, 0, 0, 0, 0],
-    [0, 1, 1, 0, 0, 0, 0, 0],
-    [1, 1, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 1, 1, 0, 0, 0, 0],
-    [0, 1, 1, 0, 0, 0, 0, 0],
-    [1, 1, 0, 0, 0, 0, 0, 0],
-    [1, 1, 1, 1, 1, 1, 1, 1]
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0]
 ]
 
 lst = [i*Nlin+j for i in range(nlin) for j in range(ncol) if template[i][j]]
