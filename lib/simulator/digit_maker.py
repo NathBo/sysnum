dbd39f2d0dead96b03d0171d9c6cd5c4014831f4
mateo713 <68335444+mateo713@users.noreply.github.com>
2024-01-22 13:07:29 +0100

    Update digit_maker.py

--- lib/simulator/digit_maker.py
+++ lib/simulator/digit_maker.py
@@ -414,66 +414,21 @@ lst6 = [i*Nlin+j for i in range(nlin) for j in range(ncol) if template6[i][j]]
 lst7 = [i*Nlin+j for i in range(nlin) for j in range(ncol) if template7[i][j]]
 lst8 = [i*Nlin+j for i in range(nlin) for j in range(ncol) if template8[i][j]]
 lst9 = [i*Nlin+j for i in range(nlin) for j in range(ncol) if template9[i][j]]
-w = '[' + '; '.join(map(str, lst0)) + ']'
+w = '[' + '; '.join(map(str, lst8)) + ']'
 
 lst = [lst0, lst1, lst2, lst3, lst4, lst5, lst6, lst7, lst8, lst9]
 print('\n\n'.join(map(str, lst)))
 
-"""
-open Graphics
-open Netlist_ast
-        
-let white = yellow and black = rgb 128 0 128
+print('\n'*3)
+trans = [
+    (list(set(lst[i-1]) - set(lst[i])), list(set(lst[i]) - set(lst[i-1])))
+    for i in range(10)
+]
+print('\n\n'.join(map(lambda pair: "\n".join(map(str, pair)), trans)))
 
-(*
-conventions:
-les bords font 8 megapixels  
-les megapixels sont 8x8
-un chiffre est en 12x8 megapixels 
-*)
 
-let screen_width = 992 and screen_height = 48*8
-let mpixs = 4 and edge = 4
-let m_width = screen_width/mpixs - 2*edge and m_height = screen_height/mpixs - 2*edge
-
-let screen_init () = 
-  open_graph (" " ^ (string_of_int screen_width) ^ "x" ^ (string_of_int screen_height)); (*oui, il faut un espace en premier*)
-  set_window_title "clock";
-  set_color black;
-  fill_rect 0 0 (screen_width-1) (screen_height-1);
-  auto_synchronize false
-
-let draw_ram ram = 
-  if Array.length ram <> screen_width * screen_height then failwith "image size and window size do not match";
-  fill_rect 0 0 (screen_width-1) (screen_height-1);
-  set_color white;
-  let to_display = ref [] in
-  for i = 0 to screen_height - 1 do
-    for j = 0 to screen_width - 1 do
-      if ram.(i*screen_width + j) = VBit(true) then to_display := (j, i) :: !to_display
-      done
-    done;
-  plots (Array.of_list !to_display);
-  set_color black;
-  synchronize ()
-
-let update_display to_change_white to_change_black =
-
-  let update (color, lst) = 
-    set_color color;
-    let to_display = List.map (fun i -> (mpixs * (edge+ (i mod m_width)), mpixs*(1+edge + (i/m_width)))) lst in
-    List.iter (fun (x, y) -> fill_rect x (screen_height - y) mpixs mpixs) to_display in
-  List.iter update [(white, to_change_white); (black, to_change_black)];
-  synchronize ()
-
-let get_input () = wait_next_event [Key_pressed; Poll]
-
-let tests () = 
-  screen_init ();
-  let w = {w} in
-  update_display w [];
-  Unix.sleep 60
-  
-
-let _=tests ()
-"""
+for i, l in enumerate(lst):
+    print('\n'*3)
+    print(f"display_{i}:\n")
+    print('\n'.join(map(lambda pos : f"setram rTODO rTODO {pos}", l)))
+    
